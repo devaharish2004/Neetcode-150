ea69253b2ddefd93e3151caa71393ffdc387f236
devaharish2004 <devaharishm@gmail.com>
2024-03-19 10:49:04 +0530

    Contains Duplicates

---
+++ Arrays/Contains Duplicates/Code.cpp
@@ -0,0 +1,67 @@
+//APPROACH 1 : BRUTE FORCE
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+//TC : O(N^2)
+// SC : O(1)
+
+// APPROACH 2 : SORTING
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+};
+
+// TC : O(N*logN)
+// SC : O(1)
+
+//APPROACH 3 : HASHSET
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num) > 0)
+                return true;
+            seen.insert(num);
+        }
+        return false;
+    }
+};
+
+// TC : O(N)
+// SC : O(N)
+
+//APPROACH 4 : HASHMAP / FREQUENCY ARRAY
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> seen;
+        for (int num : nums) {
+            if (seen[num] >= 1)
+                return true;
+            seen[num]++;
+        }
+        return false;
+    }
+};
+
+// TC : O(N)
+// SC : O(N)
---
+++ Arrays/Contains Duplicates/Code.java
@@ -0,0 +1,62 @@
+//APPROACH 1 : BRUTE FORCE
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
+//TC : O(N^2)
+// SC : O(1)
+
+// APPROACH 2 : SORTING
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+}
+// TC : O(N*logN)
+// SC : O(1)
+
+//APPROACH 3 : HASHSET
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num))
+                return true;
+            seen.add(num);
+        }
+        return false;
+    }
+}
+// TC : O(N)
+// SC : O(N)
+
+//APPROACH 4 : HASHMAP / FREQUENCY ARRAY
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> seen = new HashMap<>();
+        for (int num : nums) {
+            if (seen.containsKey(num) && seen.get(num) >= 1)
+                return true;
+            seen.put(num, seen.getOrDefault(num, 0) + 1);
+        }
+        return false;
+    }
+}
+// TC : O(N)
+// SC : O(N)
