018bd588e34e84e0e77d634613bbd551ca8b5708
devaharish2004 <devaharishm@gmail.com>
2024-03-19 13:09:51 +0530

    Two Sum

---
+++ Arrays/Two Sum/Code.cpp
@@ -0,0 +1,73 @@
+//APPROACH 1 : BRUTE FORCE
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // No solution found
+    }
+};
+//TC : O(N^2)
+//SC : O(1)
+
+//APPROACH 2 : TWO-PASS HASH TABLE
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        // Build the hash table
+        for (int i = 0; i < n; i++) {
+            numMap[nums[i]] = i;
+        }
+
+        // Find the complement
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            //check if the complement exists and the complement's index is not equal to the current index (as we need a pair of elements)
+            if (numMap.count(complement) && numMap[complement] != i) {
+                return {i, numMap[complement]};
+            }
+        }
+
+        return {}; // No solution found
+    }
+};
+
+//TC : O(2N)
+// SC : O(N)
+
+//APPROACH 3 : SINGLE PASS HASH SOLUTION
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; // No solution found
+    }
+};
+
+//TC : O(N)
+// SC : O(N)
+
+/*
+These solutions are to return the indices of the pairs that sum to a number.
+If we just want to check if two pairs exists, we can use the two pointer approach after sorting the array.
+*/
+
---
+++ Arrays/Two Sum/Code.java
@@ -0,0 +1,69 @@
+//APPROACH 1 : BRUTE FORCE
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; // No solution found
+    }
+}
+//TC : O(N^2)
+//SC : O(1)
+
+//APPROACH 2 : TWO-PASS HASH TABLE
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        // Build the hash table
+        for (int i = 0; i < n; i++) {
+            numMap.put(nums[i], i);
+        }
+
+        // Find the complement
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+                return new int[]{i, numMap.get(complement)};
+            }
+        }
+
+        return new int[]{}; // No solution found
+    }
+}
+
+//TC : O(2N)
+// SC : O(N)
+
+//APPROACH 3 : SINGLE PASS HASH SOLUTION
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{}; // No solution found
+    }
+}
+
+//TC : O(N)
+// SC : O(N)
+
+/*
+These solutions are to return the indices of the pairs that sum to a number.
+If we just want to check if two pairs exists, we can use the two pointer approach after sorting the array.
+*/
+
