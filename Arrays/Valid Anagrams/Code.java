408ad48fa90ed9dc68f5bf4b4d1cde396e2732b3
devaharish2004 <devaharishm@gmail.com>
2024-03-19 11:29:23 +0530

    Valid Anagrams

---
+++ Arrays/Valid Anagrams/Code.java
@@ -0,0 +1,75 @@
+//APPROACH 1 : SORTING
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
+    }
+}
+
+//TC : O(NlogN)
+// SC : O(1)
+
+//APPROACH 2 : HASHMAP
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> count = new HashMap<>();
+        
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) - 1);
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+//TC : O(N)
+//SC : O(N)
+
+//APPROACH 3 : HASHMAP USING ARRAY
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+        
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+//TC : O(N)
+//SC : O(26) ~ O(1)
